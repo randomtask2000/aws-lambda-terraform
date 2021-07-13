48e0d231fca8925f1851462331ed24df1fd479ba
Emilio Nicoli <emilionicoli@gmail.com>
2021-07-13 14:33:39 -0600

    add dotnet build and terraform apply script

--- java/terraform/main.tf
+++ java/terraform/main.tf
@@ -3,12 +3,15 @@ provider "aws" {
   /*
   You can set and uncomment these or apply them with EXPORTs to your terminal session.
 
+cat << EOF > terraform.tfvars
   aws_access_key = "<< your IAM user AWS access key >>"
   aws_secret_key = "<< your IAM user AWS secret key >>"
   account_id = "<<your AWS account id>>"
+EOF
 
   export AWS_ACCESS_KEY_ID="<< your IAM user AWS access key >>"
   export AWS_SECRET_ACCESS_KEY="<< your IAM user AWS secret key >>"
   export AWS_SESSION_TOKEN="<< your session token when needed >>"
+
   */
 }
