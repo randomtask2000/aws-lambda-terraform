3aca6b039554976c618964a6c71273578fb8f6dd
Emilio Nicoli <emilionicoli@gmail.com>
2021-07-13 14:31:11 -0600

    add dotnet build script

--- dotnet/build_and_deploy.sh
+++ dotnet/build_and_deploy.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-cd helloworldjava
-printf '\n\nBuilding the Java Lambda Function!\n\n'
-#mvn clean verify
-gradle build --build-cache --info
-#gradle clean verify
+curdir = $PWD
+printf '\n\nBuilding the Dotnet Lambda Function!\n\n'
+dotnet build "$PWD/helloworlddotnet/HelloWorld/src/HelloWorld/HelloWorld.csproj"
 if [ $? -ne 0 ]; then
-  printf '\n\nJava application build failed! No new Lambda Function will be deployed!!!i\n'
+  printf '\n\nDotnet application build failed! No new Lambda Function will be deployed!!!i\n'
   exit -1
 fi
-
-cd ..
+cd curdir
 printf '\n\nStarting the Terraforming!\n\n'
 cd terraform
 terraform plan -out=plan.out
