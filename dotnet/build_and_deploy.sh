eaf1c2b247bc358353aa63b201ab4700b65de9bb
Emilio Nicoli <emilionicoli@gmail.com>
2021-07-14 14:05:44 -0600

    more readme content and dotnet shell script changes

--- dotnet/build_and_deploy.sh
+++ dotnet/build_and_deploy.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-curdir = $PWD
 printf '\n\nBuilding the Dotnet Lambda Function!\n\n'
 dotnet build "$PWD/helloworlddotnet/HelloWorld/src/HelloWorld/HelloWorld.csproj"
 if [ $? -ne 0 ]; then
   printf '\n\nDotnet application build failed! No new Lambda Function will be deployed!!!i\n'
   exit -1
 fi
-cd curdir
+
 printf '\n\nStarting the Terraforming!\n\n'
 cd terraform
 terraform plan -out=plan.out
